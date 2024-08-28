53301b51ae3823eff0b9fe63fdec680217a41f3e
aemckenna <mr.ashermckenna@gmail.com>
2024-08-28 18:08:28 -0400

    folders

--- RecipeMaker.py
+++ RecipeMaker.py
@@ -1,10 +1,11 @@
 from flask import Flask
-from openai import OpenAI
-client = OpenAI()
+import openai
+
+# Set your OpenAI API key
+client = openai.OpenAI(api_key="sk--ygmOsjWiF5YF7PDdBk_U-mvaapEI37eZgXkEblZRmT3BlbkFJ9zdJc0XJEnjPFwXPQOtDQ5pSp87cDIk-ZBx3KQpr8A")
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
-
